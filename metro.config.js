31e4837b16e0b260985673616de8f8d86ff3dede
zhiqingchen <tony.zq.chen@gmail.com>
2023-07-18 14:57:30 +0800

    feat: rn 72

--- metro.config.js
+++ metro.config.js
@@ -1,3 +1,12 @@
-module.exports = {
-  // resetCache: true
-};
+const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
+const { getMetroConfig } = require('@tarojs/rn-supporter')
+
+/**
+ * Metro configuration
+ * https://facebook.github.io/metro/docs/configuration
+ *
+ * @type {import('metro-config').MetroConfig}
+ */
+const config = {};
+
+module.exports = mergeConfig(getDefaultConfig(__dirname), getMetroConfig(), config)
